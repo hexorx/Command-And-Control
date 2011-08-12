a74ddbf6631edf5fc343514d7f499417376a955e
Josh Robinson <hexorx@gmail.com>
2011-08-12 11:00:58 -0600

    Fixed Inclusion of ClassMethods for ControllerAdditions

--- lib/c2/controller_additions.rb
+++ lib/c2/controller_additions.rb
@@ -5,32 +5,34 @@ module C2
       base.helper_method :current_c2_agent, :current_c2_agent_title, :c2_login_path, :c2_profile_path, :c2_logout_path
     end
     
-    def current_c2_agent
-      @current_c2_agent ||= current_user
-    end
+    module ClassMethods
+      def current_c2_agent
+        @current_c2_agent ||= current_user
+      end
     
-    def current_c2_agent_title
-      @current_c2_agent_title ||= current_c2_agent.send(([:c2_title, :title, :name, :email].map(&:to_s) & current_c2_agent.methods).first)
-    end
+      def current_c2_agent_title
+        @current_c2_agent_title ||= current_c2_agent.send(([:c2_title, :title, :name, :email].map(&:to_s) & current_c2_agent.methods).first)
+      end
     
-    def c2_clearance?
-      current_c2_agent && current_c2_agent.admin?
-    end
+      def c2_clearance?
+        current_c2_agent && current_c2_agent.admin?
+      end
     
-    def authorize_c2_agent
-      raise C2::AccessDenied unless c2_clearance?
-    end
+      def authorize_c2_agent
+        raise C2::AccessDenied unless c2_clearance?
+      end
     
-    def c2_login_path
-      '/'
-    end
+      def c2_login_path
+        '/'
+      end
     
-    def c2_profile_path
-      '/'
-    end
+      def c2_profile_path
+        '/'
+      end
     
-    def c2_logout_path
-      '/'
+      def c2_logout_path
+        '/'
+      end
     end
   end
 end
